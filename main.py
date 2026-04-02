b7d10a11eb8527ff74a88a1a54eae5e53baae8d2
Matheus Fabião <matheusfabiao.dev@gmail.com>
2026-04-02 18:37:20 -0300

    feat: added day_of_week parameter in cron job settings

--- main.py
+++ main.py
@@ -9,7 +9,13 @@ scheduler = BlockingScheduler(timezone=ZoneInfo('America/Sao_Paulo'))
 bcb_service = BcbService()
 logger = Logger(__name__)
 
-scheduler.add_job(bcb_service.handle_data, trigger='cron', hour='6-22', minute='*/10')
+scheduler.add_job(
+    bcb_service.handle_data,
+    trigger='cron',
+    hour='6-21',
+    minute='*/10',
+    day_of_week='mon-fri',
+)
 
 if __name__ == '__main__':
     try:
